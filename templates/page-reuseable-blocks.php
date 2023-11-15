076a3c7caae954dba3ec97d7545c58a137687036
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-15 23:41:42 +0800

    css

--- templates/page-reuseable-blocks.php
+++ templates/page-reuseable-blocks.php
@@ -13,13 +13,18 @@ $footer_type = carbon_get_the_post_meta('footer_type');
 ?>
 <?php get_header(); ?>
 <main id="page-components" class="page-components reusable-blocks <?= $class ?> <?= $main_class ?>">
-    <section class="hero-banner-with-breadcrumbs">
+    <section class="hero-banner-with-breadcrumbs d-flex align-items-end">
         <div class="video-holder">
             <iframe frameborder="0" height="100%" width="100%" src="https://www.youtube.com/embed/<?= $embed_id ?>?autoplay=1&mute=1&controls=0&showinfo=0&autohide=1&loop=1&rel=0&playlist=q6CEBGW4szM">
             </iframe>
         </div>
         <div class="container">
             <?= breadcrumbs() ?>
+            <div class="heading-box">
+                <h1>
+                    <?php the_title() ?>
+                </h1>
+            </div>
         </div>
     </section>
 </main>
