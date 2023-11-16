d10b6d96d723eab4006f4d5092fdb3ad03b0dab3
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-16 23:52:21 +0800

    Update main.js

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -400,7 +400,7 @@ function bike_scroller() {
 
 			if (stickyTop < windowTop) {
 				jQuery('body').addClass('bike-scroller-active');
-				jQuery('.bike-scroller .row-images').css('transform', 'translateX(' + (100 * $transform) + 'px)');
+				jQuery('.bike-scroller .row-images').css('transform', 'translateX(-' + (100 * $transform) + 'px)');
 				if (windowTop > lastScrollTop) {
 					$transform++;
 				} else {
