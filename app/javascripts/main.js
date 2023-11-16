d1046b17710775545b782def1783d67747c3c602
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-16 22:47:27 +0800

    css

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -387,7 +387,8 @@ function bike_navigation() {
 
 function bike_scroller() {
 	jQuery('.horizontal-scroll').horizontalScroll({
-		containerHeight: "100vh"
+		containerHeight: "100vh",
+		width: "50vw"
 	});
 }
 function bike_specs() {
