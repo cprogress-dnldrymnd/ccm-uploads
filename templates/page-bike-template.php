719a2cc23eb2263641949c5f35e88e712b6c54fe
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-17 01:03:56 +0800

    css

--- templates/page-bike-template.php
+++ templates/page-bike-template.php
@@ -25,7 +25,15 @@ $footer_type = carbon_get_the_post_meta('footer_type');
                     <?php the_title() ?>
                 </h1>
             </div>
+            <div class="next-section">
+                <a href="#overview" class="d-flex align-items-center justify-content-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
+                    </svg>
+                </a>
+            </div>
         </div>
+
     </section>
     <section class="bike-navigation">
         <div class="inner">
@@ -40,13 +48,13 @@ $footer_type = carbon_get_the_post_meta('footer_type');
                         <div class="bike-nav">
                             <ul class="list-inline text-center">
                                 <li>
-                                    <a href="#">OVERVIEW</a>
+                                    <a href="#overview">OVERVIEW</a>
                                 </li>
                                 <li>
-                                    <a href="#">FEATURES</a>
+                                    <a href="#features">FEATURES</a>
                                 </li>
                                 <li>
-                                    <a href="#">SPECIFICATION</a>
+                                    <a href="#bike-specification">SPECIFICATION</a>
                                 </li>
                             </ul>
                         </div>
@@ -76,7 +84,7 @@ $footer_type = carbon_get_the_post_meta('footer_type');
         </div>
     </section>
 
-    <section class="overview">
+    <section class="overview" id="overview">
         <div class="container">
             <div class="inner">
                 <div class="heading-box">
@@ -99,7 +107,7 @@ $footer_type = carbon_get_the_post_meta('footer_type');
         </div>
     </section>
 
-    <section class="features">
+    <section class="features" id="features">
         <div class="inner">
             <div class="container">
                 <div class="row align-items-center gy-3">
@@ -164,13 +172,13 @@ $footer_type = carbon_get_the_post_meta('footer_type');
         <div class="scroll-offset"></div>
     </section>
 
-    <section class="bike-specification">
+    <section class="bike-specification" id="bike-specification">
         <div class="container">
             <div class="specs-holder">
                 <div class="specs-box">
                     <div class="specs-heading">
                         <h3>
-                            ENGINE 
+                            ENGINE
                         </h3>
                         <span class="icon"></span>
                     </div>
@@ -214,7 +222,7 @@ $footer_type = carbon_get_the_post_meta('footer_type');
                 <div class="specs-box">
                     <div class="specs-heading">
                         <h3>
-                            STATIC DIMENSIONS 
+                            STATIC DIMENSIONS
                         </h3>
                         <span class="icon"></span>
                     </div>
