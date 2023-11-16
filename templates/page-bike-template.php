d7d06c0c34e2c90644b5ddf76f56dc74a1fc1499
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-17 01:04:43 +0800

    css

--- templates/page-bike-template.php
+++ templates/page-bike-template.php
@@ -13,24 +13,26 @@ $footer_type = carbon_get_the_post_meta('footer_type');
 ?>
 <?php get_header(); ?>
 <main id="page-components" class="main-holder reusable-blocks bt-5 <?= $class ?> <?= $main_class ?>">
-    <section class="hero-banner-with-breadcrumbs d-flex align-items-end justify-content-between">
+    <section class="hero-banner-with-breadcrumbs d-flex align-items-end ">
         <div class="video-holder">
             <iframe frameborder="0" height="100%" width="100%" src="https://www.youtube.com/embed/<?= $embed_id ?>?autoplay=1&mute=1&controls=0&showinfo=0&autohide=1&loop=1&rel=0&playlist=q6CEBGW4szM">
             </iframe>
         </div>
         <div class="container-fluid container-fluid-wide content">
             <?= breadcrumbs() ?>
-            <div class="heading-box">
-                <h1>
-                    <?php the_title() ?>
-                </h1>
-            </div>
-            <div class="next-section">
-                <a href="#overview" class="d-flex align-items-center justify-content-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
-                    </svg>
-                </a>
+            <div class="inner d-flex justify-content-between align-items-end">
+                <div class="heading-box">
+                    <h1>
+                        <?php the_title() ?>
+                    </h1>
+                </div>
+                <div class="next-section">
+                    <a href="#overview" class="d-flex align-items-center justify-content-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
 
