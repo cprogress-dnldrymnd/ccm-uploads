966da959461f3f7b0108f6044da0b9c12fc40da7
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-16 20:47:53 +0800

    Update main.js

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -363,14 +363,9 @@ jQuery(window).on('resize', function () {
 function bike_navigation() {
 	if (jQuery('.bike-navigation').length > 0) {
 		$bike_nav_height = jQuery('.bike-navigation .inner').outerHeight();
-		$header_height = jQuery('#ccm-motors-header').outerHeight();
-
 		jQuery('.bike-navigation').css('height', $bike_nav_height + 'px');
 		var stickyTop = jQuery('.bike-navigation').offset().top;
-		if (jQuery('body').hasClass('show-header')) {
-			stickyTop = stickyTop + $header_height;
-		}
-
+	
 		jQuery(window).scroll(function () {
 			var windowTop = jQuery(window).scrollTop();
 
