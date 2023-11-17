eef2c358926292bb1b31e46e969f978c882b6751
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-17 18:34:55 +0800

    cs

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -487,10 +487,10 @@ function header() {
 		jQuery(this).next().toggleClass('show-dropdown');
 		if (!jQuery(this).next().hasClass('show-dropdown')) {
 			jQuery('.overlay').removeClass('show-overlay');
-			jQuery('body').removeClass('mega-menu-active');
+			jQuery('#ccm-motors-header').removeClass('mega-menu-active');
 		} else {
 			jQuery('.overlay').addClass('show-overlay');
-			jQuery('body').addClass('mega-menu-active');
+			jQuery('#ccm-motors-header').addClass('mega-menu-active');
 
 		}
 		e.preventDefault();
