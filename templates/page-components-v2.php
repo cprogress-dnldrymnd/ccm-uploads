f9a310f4ba4c551b267b0d06f7adb8f58bdd2423
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-22 21:33:50 +0800

    css

--- templates/page-components-v2.php
+++ templates/page-components-v2.php
@@ -73,6 +73,34 @@ $alt_title = carbon_get_the_post_meta('alt_title');
             </div>
         </div>
     </section>
+    <section class="two-columns-8-4">
+        <div class="container">
+            <div class="inner">
+                <div class="row g-5 row-reverse">
+                    <div class="col-lg-8">
+                        <div class="image-box">
+                            <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/ccm-insurance.png" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-4 content-margin">
+                        <div class="heading-box">
+                            <h3>
+                                HEADING THREE
+                            </h3>
+                        </div>
+                        <div class="description-box">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna lorem, ultricies eget pretium et, suscipit tincidunt libero.
+                            </p>
+                        </div>
+                        <div class="btn-box btn-bordered">
+                            <a href="#" class="pc-btn">CALL TO ACTION</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <section class="footer-buttons">
         <div class="row g-0">
             <div class="col-6">
