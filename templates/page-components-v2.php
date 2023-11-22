bfd3add7ce503d9ece46c6687fb9ec315ea76fda
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-22 21:23:40 +0800

    css

--- templates/page-components-v2.php
+++ templates/page-components-v2.php
@@ -45,6 +45,32 @@ $alt_title = carbon_get_the_post_meta('alt_title');
             </div>
         </div>
     </section>
+    <section class="two-columns-8-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="image-box">
+                        <img src="" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="heading-box">
+                        <h3>
+                            HEADING THREE
+                        </h3>
+                    </div>
+                    <div class="description-box">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna lorem, ultricies eget pretium et, suscipit tincidunt libero.
+                        </p>
+                    </div>
+                    <div class="btn-box btn-bordered">
+                        <a href="#">CALL TO ACTION</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <section class="footer-buttons">
         <div class="row g-0">
             <div class="col-6">
