ad6bda498a4e3e3a0b170730eb843111606eb807
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-22 21:45:19 +0800

    Update page-components-v2.php

--- templates/page-components-v2.php
+++ templates/page-components-v2.php
@@ -28,100 +28,103 @@ $alt_title = carbon_get_the_post_meta('alt_title');
             </div>
         </div>
     </section>
-    <section class="wysiwyg-v2">
-        <div class="container">
-            <div class="inner text-center content-margin">
-                <h2>
-                    NOT YOUR TYPICAL MOTORCYCLE INSURANCE
-                </h2>
-                <p>
-                    CCM Motorcycles have teamed up with two of the greatest names in motorcycling to bring you the very best solutions to insure your CCM! We ensure that they understand your CCM model meaning no more long pauses as you explain what a Spitfire is!
-                </p>
-                <div class="btn-box">
-                    <a class="pc-btn" href="<?= $configure_url ?>" data-target="#enquire-now">
-                        GET A QUOTE
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="two-columns-8-4">
-        <div class="container">
-            <div class="inner">
-                <div class="row g-5">
-                    <div class="col-lg-8">
-                        <div class="image-box">
-                            <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/ccm-insurance.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col-lg-4 content-margin">
-                        <div class="heading-box">
-                            <h3>
-                                HEADING THREE
-                            </h3>
-                        </div>
-                        <div class="description-box">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna lorem, ultricies eget pretium et, suscipit tincidunt libero.
-                            </p>
-                        </div>
-                        <div class="btn-box btn-bordered">
-                            <a href="#" class="pc-btn">CALL TO ACTION</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="two-columns-8-4">
-        <div class="container">
-            <div class="inner">
-                <div class="row g-5 row-reverse">
-                    <div class="col-lg-8">
-                        <div class="image-box">
-                            <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/ccm-insurance.png" alt="">
-                        </div>
-                    </div>
-                    <div class="col-lg-4 content-margin">
-                        <div class="heading-box">
-                            <h3>
-                                HEADING THREE
-                            </h3>
-                        </div>
-                        <div class="description-box">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna lorem, ultricies eget pretium et, suscipit tincidunt libero.
-                            </p>
-                        </div>
-                        <div class="btn-box btn-bordered">
-                            <a href="#" class="pc-btn">CALL TO ACTION</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="background-section" style="background-image: url(https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-1.png)">
-        <div class="container">
-            <div class="inner text-center content-margin">
-                <div class="heading-box">
-                    <h2 class="mt-0">
-                        LOREM IPSUM DOLOR SIT
+    <?php if (is_page(17267)) { ?>
+        <section class="wysiwyg-v2">
+            <div class="container">
+                <div class="inner text-center content-margin">
+                    <h2>
+                        NOT YOUR TYPICAL MOTORCYCLE INSURANCE
                     </h2>
-                </div>
-                <div class="description-box">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna lorem, ultricies eget pretium et, suscipit tincidunt libero. Quisque vehicula volutpat diam. Morbi magna sem, viverra non euismod at, elementum in nisl.
+                        CCM Motorcycles have teamed up with two of the greatest names in motorcycling to bring you the very best solutions to insure your CCM! We ensure that they understand your CCM model meaning no more long pauses as you explain what a Spitfire is!
                     </p>
-                </div>
-                <div class="btn-box">
-                    <a class="pc-btn" href="<?= $configure_url ?>" data-target="#enquire-now">
-                        GET A QUOTE
-                    </a>
+                    <div class="btn-box">
+                        <a class="pc-btn" href="<?= $configure_url ?>" data-target="#enquire-now">
+                            GET A QUOTE
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+        <section class="two-columns-8-4">
+            <div class="container">
+                <div class="inner">
+                    <div class="row g-5">
+                        <div class="col-lg-8">
+                            <div class="image-box">
+                                <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/ccm-insurance.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-4 content-margin">
+                            <div class="heading-box">
+                                <h3>
+                                    HEADING THREE
+                                </h3>
+                            </div>
+                            <div class="description-box">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna lorem, ultricies eget pretium et, suscipit tincidunt libero.
+                                </p>
+                            </div>
+                            <div class="btn-box btn-bordered">
+                                <a href="#" class="pc-btn">CALL TO ACTION</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="two-columns-8-4">
+            <div class="container">
+                <div class="inner">
+                    <div class="row g-5 row-reverse">
+                        <div class="col-lg-8">
+                            <div class="image-box">
+                                <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/ccm-insurance.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-4 content-margin">
+                            <div class="heading-box">
+                                <h3>
+                                    HEADING THREE
+                                </h3>
+                            </div>
+                            <div class="description-box">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna lorem, ultricies eget pretium et, suscipit tincidunt libero.
+                                </p>
+                            </div>
+                            <div class="btn-box btn-bordered">
+                                <a href="#" class="pc-btn">CALL TO ACTION</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="background-section" style="background-image: url(https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-1.png)">
+            <div class="container">
+                <div class="inner text-center content-margin">
+                    <div class="heading-box">
+                        <h2 class="mt-0">
+                            LOREM IPSUM DOLOR SIT
+                        </h2>
+                    </div>
+                    <div class="description-box">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna lorem, ultricies eget pretium et, suscipit tincidunt libero. Quisque vehicula volutpat diam. Morbi magna sem, viverra non euismod at, elementum in nisl.
+                        </p>
+                    </div>
+                    <div class="btn-box">
+                        <a class="pc-btn" href="<?= $configure_url ?>" data-target="#enquire-now">
+                            GET A QUOTE
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php } else { ?>
+    <?php } ?>
     <section class="footer-buttons">
         <div class="row g-0">
             <div class="col-6">
