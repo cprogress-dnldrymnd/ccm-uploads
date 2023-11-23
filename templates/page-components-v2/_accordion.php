13c37096dcc5aab616dcdac39c68561a6bf3616f
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-23 18:47:20 +0800

    cs

---
+++ templates/page-components-v2/_accordion.php
@@ -0,0 +1,64 @@
+<section class="accordion-section">
+    <div class="container">
+        <div class="inner">
+            <div class="row gy-4">
+                <div class="col-lg-6">
+                    <div class="inner">
+                        <div class="heading-box">
+                            <h2>FREQUENTLY<br> ASKED QUESTIONS</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="accordion-holder">
+                        <div class="accordion-item">
+                            <div class="accordion-heading">
+                                <h3>
+                                    FREQUENTLY ASKED QUESTION
+                                </h3>
+                                <span class="icon"></span>
+                            </div>
+                            <div class="accordion-content">
+                                <div class="inner">
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis commodo diam, a aliquam ex posuere ac. Donec posuere tincidunt hendrerit. Aenean euismod, sapien in vestibulum posuere, metus massa viverra urna, id aliquam turpis magna at lorem. Nullam pharetra neque eu diam ornare ornare.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-heading">
+                                <h3>
+                                    FREQUENTLY ASKED QUESTION
+                                </h3>
+                                <span class="icon"></span>
+                            </div>
+                            <div class="accordion-content">
+                                <div class="inner">
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis commodo diam, a aliquam ex posuere ac. Donec posuere tincidunt hendrerit. Aenean euismod, sapien in vestibulum posuere, metus massa viverra urna, id aliquam turpis magna at lorem. Nullam pharetra neque eu diam ornare ornare.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-heading">
+                                <h3>
+                                    FREQUENTLY ASKED QUESTION
+                                </h3>
+                                <span class="icon"></span>
+                            </div>
+                            <div class="accordion-content">
+                                <div class="inner">
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis commodo diam, a aliquam ex posuere ac. Donec posuere tincidunt hendrerit. Aenean euismod, sapien in vestibulum posuere, metus massa viverra urna, id aliquam turpis magna at lorem. Nullam pharetra neque eu diam ornare ornare.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ templates/page-components-v2/_contact_form.php
@@ -0,0 +1,31 @@
+<section class="overview contact-form-holder v2">
+    <div class="container">
+        <div class="row gy-4">
+            <div class="col-lg-6">
+                <div class="inner">
+                    <div class="heading-box">
+                        <h2>BOOK A SERVICE</h2>
+                    </div>
+                    <div class="description-box">
+                        <p>
+                            Please fill in your details and one of our team members will contact you within 48
+                            hours.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="contact-form">
+                    <script charset="utf-8" type="text/javascript" src="//js-eu1.hsforms.net/forms/embed/v2.js"></script>
+                    <script>
+                        hbspt.forms.create({
+                            region: "eu1",
+                            portalId: "139521183",
+                            formId: "c45f8060-df31-4ff7-bdf3-39a8a20823e1"
+                        });
+                    </script>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
