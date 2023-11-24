7c83bd129a00d04b71d0c1f3ad2ab475cfe1e4e9
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-24 23:17:48 +0800

    Update page-bikes.php

--- templates/page-bikes.php
+++ templates/page-bikes.php
@@ -59,11 +59,24 @@ $bikes = carbon_get_the_post_meta('bikes');
             <div class="swiper-pagination"></div>
             <div class="row">
                 <div class="col-auto">
-                    <div class="swiper-button-next-custom"></div>
+                    <div class="swiper-button-prev-custom">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                        </svg>
+                    </div>
                 </div>
                 <div class="col-auto">
-                    <div class="swiper-button-prev-custom"></div>
+                    <div class="swiper-button-next-custom">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-arrow-left" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
+                        </svg>
+                    </div>
                 </div>
+
             </div>
         </div>
     </section>
@@ -108,6 +121,10 @@ $bikes = carbon_get_the_post_meta('bikes');
         loop: true,
         centeredSlides: true,
         parallax: true,
+        navigation: {
+            nextEl: ".swiper-button-next-custom",
+            prevEl: ".swiper-button-prev-custom",
+        },
         pagination: {
             el: '.swiper-pagination',
             type: 'fraction',
