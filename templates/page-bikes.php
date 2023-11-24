d29c15c4faad28cd1ad48e3b3f10741fbeb81476
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-24 20:02:20 +0800

    cs

--- templates/page-bikes.php
+++ templates/page-bikes.php
@@ -14,38 +14,36 @@ $alt_title = carbon_get_the_post_meta('alt_title');
 ?>
 <main id="page-components" class="main-holder main-holder-v2  bt-5">
     <section class="slider-hero">
-        <div class="container">
-            <div class="swiper-container mySwiper">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="image-box">
-                            <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
-                        </div>
+        <div class="swiper-container mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="image-box">
+                        <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
                     </div>
-                    <div class="swiper-slide">
-                        <div class="image-box">
-                            <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
-                        </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="image-box">
+                        <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
                     </div>
-                    <div class="swiper-slide">
-                        <div class="image-box">
-                            <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
-                        </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="image-box">
+                        <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
                     </div>
-                    <div class="swiper-slide">
-                        <div class="image-box">
-                            <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
-                        </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="image-box">
+                        <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
                     </div>
-                    <div class="swiper-slide">
-                        <div class="image-box">
-                            <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
-                        </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="image-box">
+                        <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
                     </div>
-                    <div class="swiper-slide">
-                        <div class="image-box">
-                            <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
-                        </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="image-box">
+                        <img src="https://ccm.theprogressteam.com/wp-content/uploads/2020/11/Maverick_Black_Transparent-_Shadow.png" alt="">
                     </div>
                 </div>
             </div>
