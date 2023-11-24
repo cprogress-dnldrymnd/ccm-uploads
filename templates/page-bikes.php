31a33a20d2d1fe41e5bd4787dde2faccdb33a0a8
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-24 20:31:15 +0800

    Update page-bikes.php

--- templates/page-bikes.php
+++ templates/page-bikes.php
@@ -58,6 +58,35 @@ $bikes = carbon_get_the_post_meta('bikes');
             </div>
         </div>
     </section>
+
+    <section class="bike-lists-menu bike-lists-menu-page">
+        <div class="container bike-lists-holder">
+            <div class="row g-0 bikes">
+                <?php foreach ($bikes as $bike) { ?>
+                    <?php
+                    $title = get_the_title($bike['id']);
+                    $link = get_permalink($bike['id']);
+                    $bike_tagline = carbon_get_post_meta($bike['id'], 'bike_tagline');
+                    $bike_slider_image_bike_page = carbon_get_post_meta($bike['id'], 'bike_slider_image_bike_page');
+                    $bike_menu_image = carbon_get_post_meta($bike['id'], 'bike_menu_image');
+                    $tagline = carbon_get_post_meta($bike['id'], 'tagline');
+                    $bike_image = $bike_menu_image ? $bike_menu_image : $bike_slider_image_bike_page;
+                    $bike_image_url = wp_get_attachment_image_url($bike_image, 'large');
+                    ?>
+                    <div class="col-lg-4">
+                        <div class="column-holder">
+                            <div class="bike-name">
+                                <h4><?= $title ?></h4>
+                            </div>
+                            <div class="image-box">
+                                <img src="<?= $bike_image_url ?>" alt="<?= $title ?>">
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </section>
     <?php while (have_posts()) {
         the_post();
     }
