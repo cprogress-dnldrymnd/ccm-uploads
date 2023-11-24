ac76c0491c1d9bfd9fc7a3c829fffdc08707919f
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-24 22:58:54 +0800

    Update page-bikes.php

--- templates/page-bikes.php
+++ templates/page-bikes.php
@@ -59,42 +59,6 @@ $bikes = carbon_get_the_post_meta('bikes');
             <div class="swiper-pagination"></div>
         </div>
     </section>
-
-    <section class="bike-lists-menu bike-lists-menu-page">
-        <div class="container bike-lists-holder">
-            <div class="row g-0 bikes">
-                <?php foreach ($bikes as $key => $bike) { ?>
-                    <?php
-                    $title = get_the_title($bike['id']);
-                    $link = get_permalink($bike['id']);
-                    $bike_tagline = carbon_get_post_meta($bike['id'], 'bike_tagline');
-                    $bike_slider_image_bike_page = carbon_get_post_meta($bike['id'], 'bike_slider_image_bike_page');
-                    $bike_slider_image = carbon_get_post_meta($bike['id'], 'bike_slider_image');
-                    $tagline = carbon_get_post_meta($bike['id'], 'tagline');
-                    $bike_image = $bike_slider_image_bike_page ? $bike_slider_image_bike_page : $bike_slider_image;
-                    $bike_image_url = wp_get_attachment_image_url($bike_image, 'large');
-                    ?>
-                    <div class="col-lg-4">
-                        <div class="column-holder text-center bike-<?= $key ?>">
-                            <a href="<?= $link ?>">
-                                <div class="bike-name">
-                                    <h4><?= $title ?></h4>
-                                </div>
-                                <div class="image-box">
-                                    <img src="<?= $bike_image_url ?>" alt="<?= $title ?>">
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                <?php } ?>
-            </div>
-        </div>
-    </section>
-    <?php while (have_posts()) {
-        the_post();
-    }
-    ?>
-
 </main>
 <?php get_footer(); ?>
 
