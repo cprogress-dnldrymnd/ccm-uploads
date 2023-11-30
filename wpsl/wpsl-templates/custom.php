813219f2d646d5669140d14515760dd7a1fa1252
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-01 01:26:34 +0800

    Update custom.php

--- wpsl/wpsl-templates/custom.php
+++ wpsl/wpsl-templates/custom.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 global $wpsl_settings, $wpsl;
 
-$output         = $this->get_custom_css(); 
-$autoload_class = ( !$wpsl_settings['autoload'] ) ? 'class="wpsl-not-loaded"' : '';
+$output         = $this->get_custom_css();
+$autoload_class = (!$wpsl_settings['autoload']) ? 'class="wpsl-not-loaded"' : '';
 
 
-    
-$output .= "\t" . '<div id="wpsl-gmap" class="wpsl-gmap-canvas"></div>' . "\r\n";
+$output .= '<div class="row">';
+$output .= "\t" . '<div class="col-lg-4">';
 
 $output .= "\t" . '<div id="wpsl-result-list">' . "\r\n";
 
@@ -18,15 +18,15 @@ $output .= "\t" . '<div class="wpsl-search wpsl-clearfix ' . $this->get_css_clas
 $output .= "\t\t" . '<div id="wpsl-search-wrap">' . "\r\n";
 $output .= "\t\t\t" . '<form autocomplete="off">' . "\r\n";
 $output .= "\t\t\t" . '<div class="wpsl-input">' . "\r\n";
-$output .= "\t\t\t\t" . '<input id="wpsl-search-input" placeholder="ENTER LOCATION OR POSTCODE" type="text" value="' . apply_filters( 'wpsl_search_input', '' ) . '" name="wpsl-search-input" placeholder="" aria-required="true" />' . "\r\n";
+$output .= "\t\t\t\t" . '<input id="wpsl-search-input" placeholder="ENTER LOCATION OR POSTCODE" type="text" value="' . apply_filters('wpsl_search_input', '') . '" name="wpsl-search-input" placeholder="" aria-required="true" />' . "\r\n";
 $output .= "\t\t\t" . '</div>' . "\r\n";
 
 
-if ( $this->use_category_filter() ) {
+if ($this->use_category_filter()) {
     $output .= $this->create_category_filter();
 }
 
-$output .= "\t\t\t\t" . '<div class="wpsl-search-btn-wrap"><input id="wpsl-search-btn" type="submit" value="' . esc_attr( $wpsl->i18n->get_translation( 'search_btn_label', __( 'Search', 'wpsl' ) ) ) . '"></div>' . "\r\n";
+$output .= "\t\t\t\t" . '<div class="wpsl-search-btn-wrap"><input id="wpsl-search-btn" type="submit" value="' . esc_attr($wpsl->i18n->get_translation('search_btn_label', __('Search', 'wpsl'))) . '"></div>' . "\r\n";
 
 $output .= "\t\t" . '</form>' . "\r\n";
 $output .= "\t\t" . '</div>' . "\r\n";
@@ -34,7 +34,7 @@ $output .= "\t" . '</div>' . "\r\n";
 
 //end of
 
-$output .= "\t\t" . '<div id="wpsl-stores" '. $autoload_class .'>' . "\r\n";
+$output .= "\t\t" . '<div id="wpsl-stores" ' . $autoload_class . '>' . "\r\n";
 $output .= "\t\t\t" . '<ul></ul>' . "\r\n";
 $output .= "\t\t" . '</div>' . "\r\n";
 $output .= "\t\t" . '<div id="wpsl-direction-details">' . "\r\n";
@@ -42,10 +42,18 @@ $output .= "\t\t\t" . '<ul></ul>' . "\r\n";
 $output .= "\t\t" . '</div>' . "\r\n";
 $output .= "\t" . '</div>' . "\r\n";
 
-if ( $wpsl_settings['show_credits'] ) { 
-    $output .= "\t" . '<div class="wpsl-provided-by">'. sprintf( __( "Search provided by %sWP Store Locator%s", "wpsl" ), "<a target='_blank' href='https://wpstorelocator.co'>", "</a>" ) .'</div>' . "\r\n";
+if ($wpsl_settings['show_credits']) {
+    $output .= "\t" . '<div class="wpsl-provided-by">' . sprintf(__("Search provided by %sWP Store Locator%s", "wpsl"), "<a target='_blank' href='https://wpstorelocator.co'>", "</a>") . '</div>' . "\r\n";
 }
 
 $output .= '</div>' . "\r\n";
+$output .= "\t" . '</div>';
+
+$output .= "\t" . '<div class="col-lg-8">';
+$output .= "\t" . '<div id="wpsl-gmap" class="wpsl-gmap-canvas"></div>' . "\r\n";
+$output .= "\t" . '</div>';
+$output .= '</div>';
+
+
 
 return $output;
