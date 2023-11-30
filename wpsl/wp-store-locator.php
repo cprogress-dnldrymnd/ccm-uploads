18a5a43eb97d7afae6f5330b8fc13b656a2b8273
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-01 02:29:46 +0800

    Update wp-store-locator.php

--- wpsl/wp-store-locator.php
+++ wpsl/wp-store-locator.php
@@ -64,8 +64,6 @@ function mycustom_listing_template()
     $listing_template .= "\t\t\t\t" . '</div>';
     $listing_template .= "\t\t\t" . '</div>' . "\r\n";
     $listing_template .= "\t\t" . '</div>' . "\r\n";
-    //$listing_template .= "\t\t" . '<div class="wpsl-direction-wrap">' . "\r\n";
-
     $listing_template .= "\t\t" . '</div>' . "\r\n";
     $listing_template .= "\t\t" . '</div>' . "\r\n";
     $listing_template .= "\t" . '</li>';
@@ -83,14 +81,29 @@ function custom_info_window_template()
     global $wpsl_settings, $wpsl;
 
     $info_window_template = '<div data-store-id="<%= id %>" class="wpsl-info-window">' . "\r\n";
-    $info_window_template .= "\t\t" . '<p>' . "\r\n";
-    $info_window_template .= "\t\t\t" .  wpsl_store_header_template() . "\r\n";
-    $info_window_template .= "\t\t\t" . '<span><%= address %></span>' . "\r\n";
-    $info_window_template .= "\t\t\t" . '<% if ( address2 ) { %>' . "\r\n";
-    $info_window_template .= "\t\t\t" . '<span><%= address2 %></span>' . "\r\n";
-    $info_window_template .= "\t\t\t" . '<% } %>' . "\r\n";
-    $info_window_template .= "\t\t\t" . '<span>' . wpsl_address_format_placeholders() . '</span>' . "\r\n";
-    $info_window_template .= "\t\t" . '</p>' . "\r\n";
+    $info_window_template .= "\t\t\t" . '<div class="row align-items-center">' . "\r\n";
+    $info_window_template .= "\t\t\t\t" . '<div class="col-auto">' . "\r\n";
+    $info_window_template .= "\t\t\t\t\t" . '<%= thumb %>' . "\r\n";
+    $info_window_template .= "\t\t\t\t" . '</div>';
+    $info_window_template .= "\t\t\t\t" . '<div class="col">' . "\r\n";
+    $info_window_template .= "\t\t\t\t\t" . '<div class="listing-title">' . wpsl_store_header_template('listing') . '</div>' . "\r\n";
+    $info_window_template .= "\t\t\t\t\t" . '<div class="listing-address">';
+    $info_window_template .= "\t\t\t\t\t" . '<svg xmlns="http://www.w3.org/2000/svg" width="9.36" height="11.748" viewBox="0 0 9.36 11.748"> <g id="location" transform="translate(-4.5 -2.5)"> <path id="Path_30" data-name="Path 30" d="M9.18,13.748c2.09-2.15,4.18-4.074,4.18-6.449A4.241,4.241,0,0,0,9.18,3,4.241,4.241,0,0,0,5,7.3C5,9.674,7.09,11.6,9.18,13.748Z" transform="translate(0 0)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/> <path id="Path_31" data-name="Path 31" d="M10.791,10.583A1.791,1.791,0,1,0,9,8.791,1.791,1.791,0,0,0,10.791,10.583Z" transform="translate(-1.612 -1.612)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/> </g> </svg>';
+    // Check which header format we use
+    $info_window_template .= "\t\t\t\t\t" . '<div class="listing-address-inner">';
+    $info_window_template .= "\t\t\t\t\t" . '<span class="wpsl-street"><%= address %>,</span>' . "\r\n";
+    $info_window_template .= "\t\t\t\t\t" . '<% if ( address2 ) { %>' . "\r\n";
+    $info_window_template .= "\t\t\t\t\t" . '<span class="wpsl-street"><%= address2 %>,</span>' . "\r\n";
+    $info_window_template .= "\t\t\t\t\t" . '<% } %>' . "\r\n";
+    $info_window_template .= "\t\t\t\t\t" . '<span><%= zip %></span>' . "\r\n"; // Use the correct address format
+
+    if (!$wpsl_settings['hide_country']) {
+        $info_window_template .= "\t\t\t\t\t" . '<span class="wpsl-country"><%= country %></span>' . "\r\n";
+    }
+    $info_window_template .= "\t\t\t\t\t" . '</div>';
+    $info_window_template .= "\t\t\t\t\t" . '</div>';
+    $info_window_template .= "\t\t\t\t" . '</div>';
+    $info_window_template .= "\t\t\t" . '</div>' . "\r\n";
 
     $info_window_template .= "\t\t" . '<div class="btn-box">' . "\r\n";
     $info_window_template .= "\t" . '<a class="pc-btn">MORE INFO</a>' . "\r\n";
