ddb23d572913a248388de2959315d0f565934cc0
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-01 00:41:39 +0800

    cs

--- functions.php
+++ functions.php
@@ -3192,3 +3192,24 @@ function breadcrumbs()
 	$breadcrumbs .= '</ul></div>';
 	return $breadcrumbs;
 }
+
+
+/**Store Locator */
+add_filter( 'wpsl_templates', 'custom_templates' );
+
+function custom_templates( $templates ) {
+
+    /**
+     * The 'id' is for internal use and must be unique ( since 2.0 ).
+     * The 'name' is used in the template dropdown on the settings page.
+     * The 'path' points to the location of the custom template,
+     * in this case the folder of your active theme.
+     */
+    $templates[] = array (
+        'id'   => 'custom',
+        'name' => 'Custom template',
+        'path' => get_stylesheet_directory() . '/' . 'wpsl-templates/custom.php',
+    );
+
+    return $templates;
+}
