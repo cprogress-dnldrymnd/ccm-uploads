6d8e8e389864d68d31c78eee15cb656140ceca13
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 15:43:47 +0800

    Update hybrid-scroll.js

--- app/javascripts/hybrid-scroll.js
+++ app/javascripts/hybrid-scroll.js
@@ -28,11 +28,11 @@ jQuery('body').scroll(function () {
 function transform(section) {
     const offsetTop = section.parentElement.offsetTop;
     const scrollSection = section.querySelector('.scroll_section');
-    let percentage = ((document.scrollY - offsetTop) / document.innerHeight) * 100;
+    let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
     percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage;
     scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`;
     console.log(offsetTop);
     console.log(percentage);
-    console.log(document.scrollY);
-    console.log(document.innerHeight);
+    console.log(window.scrollY);
+    console.log(window.innerHeight);
 }
