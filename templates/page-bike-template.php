10400d17ed4f3f3042cfcc183bcc7125adf7d77d
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 15:19:19 +0800

    c

--- templates/page-bike-template.php
+++ templates/page-bike-template.php
@@ -364,38 +364,5 @@ $configure_url = carbon_get_the_post_meta('configure_url');
         </div>
     </section>
 </main>
-<script>
-    jQuery(document).ready(function() {
-        const stickySections = [...document.querySelectorAll('.sticky')];
-        let images = [
-            'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png',
-            'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png',
-            'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png',
-            'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png'
-        ];
-        images.forEach(img => {
-            stickySections.forEach(section => {
-                let image = document.createElement('img');
-                image.src = img;
-                section.querySelector('.scroll_section').appendChild(image);
-            });
-        });
-
-        window.addEventListener('scroll', (e) => {
-            for (let i = 0; i < stickySections.length; i++) {
-                transform(stickySections[i]);
-            }
-        });
-
-
-        function transform(section) {
-            const offsetTop = section.parentElement.offsetTop;
-            const scrollSection = section.querySelector('.scroll_section');
-            let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
-            percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage;
-            scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0 0)`;
-            console.log(offsetTop);
-        }
-    });
-</script>
+<script src="<?= get_stylesheet_directory_uri() . '/app/javascripts/hybrid-scroll.js' ?>"> </script>
 <?php get_footer(); ?>
