243fba7433206870d87e07afaf34a39afb44cd49
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 16:41:43 +0800

    Update page-bike-template.php

--- templates/page-bike-template.php
+++ templates/page-bike-template.php
@@ -43,6 +43,7 @@ $gallery = carbon_get_the_post_meta('gallery');
         position: absolute;
         top: 0;
         height: 100%;
+        width: <?= count($gallery) * 50 ?>vw;
         will-change: transform;
         display: flex;
         justify-content: space-between;
