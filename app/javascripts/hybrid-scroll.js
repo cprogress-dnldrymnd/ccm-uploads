8c895f73ed12e471be11185c5ca49bae97a82592
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 15:26:01 +0800

    Update hybrid-scroll.js

--- app/javascripts/hybrid-scroll.js
+++ app/javascripts/hybrid-scroll.js
@@ -1,3 +1,25 @@
-jQuery(window).scroll(function () { 
-    console.log('xxxxx')
+const stickySections = [...document.querySelectorAll('.sticky')];
+let images = [
+    'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png',
+    'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png',
+    'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png',
+    'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png'
+];
+images.forEach(img => {
+    stickySections.forEach(section => {
+        let image = document.createElement('img');
+        image.src = img;
+        section.querySelector('.scroll_section').appendChild(image);
+    });
 });
+
+
+
+function transform(section) {
+    const offsetTop = section.parentElement.offsetTop;
+    const scrollSection = section.querySelector('.scroll_section');
+    let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+    percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage;
+    scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0 0)`;
+    console.log(offsetTop);
+}
