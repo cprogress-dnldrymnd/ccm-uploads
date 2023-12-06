9e53a169c0d437f4eb28b831850a4a54cb1007d8
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 15:27:10 +0800

    Update page-bike-template.php

--- templates/page-bike-template.php
+++ templates/page-bike-template.php
@@ -25,10 +25,18 @@ $configure_url = carbon_get_the_post_meta('configure_url');
         display: none;
     }
 
+    html,
+    body,
+    main {
+        width: 100%;
+        height: 100%;
+    }
 
+    
     body {
         overflow: hidden;
     }
+
 
     .sticky_parent {
         height: 500vh;
