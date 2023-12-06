32f795fe426ea91585f3463052a7006c84e5a07e
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 17:04:51 +0800

    Update page-bike-template.php

--- templates/page-bike-template.php
+++ templates/page-bike-template.php
@@ -353,7 +353,7 @@ $contact_form = carbon_get_the_post_meta('contact_form');
     const count = '<?= count($gallery) ?>';
     const sticky_parent_height = 4 * 50;
 
-    jQuery('.sticky_parent').css('height', $sticky_parent_height + 'px');
+    jQuery('.sticky_parent').css('height', sticky_parent_height + 'px');
 
 
     images.forEach(img => {
