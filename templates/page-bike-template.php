3b4d270506555234b0647c03c6965b8692b89cd0
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 15:53:04 +0800

    cs

--- templates/page-bike-template.php
+++ templates/page-bike-template.php
@@ -362,5 +362,34 @@ $gallery = carbon_get_the_post_meta('gallery');
         </div>
     </section>
 </main>
-<script src="<?= get_stylesheet_directory_uri() . '/app/javascripts/hybrid-scroll.js' ?>"> </script>
+<script>
+    const stickySections = [...document.querySelectorAll('.sticky')];
+    let images = [
+        'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png',
+        'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png',
+        'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png',
+        'https://ccm.theprogressteam.com/wp-content/uploads/2020/11/maverick-3.png'
+    ];
+    images.forEach(img => {
+        stickySections.forEach(section => {
+            let image = document.createElement('img');
+            image.src = img;
+            section.querySelector('.scroll_section').appendChild(image);
+        });
+    });
+
+    jQuery('body').scroll(function() {
+        for (let i = 0; i < stickySections.length; i++) {
+            transform(stickySections[i]);
+        }
+    });
+
+    function transform(section) {
+        const offsetTop = section.parentElement.offsetTop;
+        const scrollSection = section.querySelector('.scroll_section');
+        let percentage = ((jQuery('body').scrollTop() - offsetTop) / window.innerHeight) * 100;
+        percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage;
+        scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`;
+    }
+</script>
 <?php get_footer(); ?>
