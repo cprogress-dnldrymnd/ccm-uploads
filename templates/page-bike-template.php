e5b0a052844dd12bfc1ae5e61700790eb5d2c360
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 14:25:19 +0800

    Update page-bike-template.php

--- templates/page-bike-template.php
+++ templates/page-bike-template.php
@@ -335,6 +335,7 @@ $configure_url = carbon_get_the_post_meta('configure_url');
         slidesPerView: 2,
         spaceBetween: 30,
         mousewheel: true,
+        loop: true,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
