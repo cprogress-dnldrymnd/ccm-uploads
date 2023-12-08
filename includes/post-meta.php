974e31c332750baeff21a01dcb4a705dd2f87d46
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-08 09:08:16 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -2016,6 +2016,9 @@ function bike_individual_product_details($bike_code, $bike_name)
 }
 
 
+$postid = $_GET['post'];
+
+
 $product_cat = get_post_meta($postid, 'product_cat', true);
 
 var_dump($product_cat);
