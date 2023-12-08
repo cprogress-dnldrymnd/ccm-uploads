5a3c9d5f3394258d2af75d998d4adb88647f6fda
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-08 10:31:02 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -2031,7 +2031,7 @@ $product_cat = $wpdb->get_results(
 			ON term_taxonomy.term_taxonomy_id = terms.term_id
 			WHERE term_relationships.object_id = $file"
 );
-$configurator = Container::make('post_meta', 'Configurator ' . $cat->name)
+$configurator = Container::make('post_meta', 'Configurator')
 	->where('post_type', '=', 'product');
 foreach ($product_cat as $cat) {
 	$configurator->add_tab($cat->name, bike_individual_product_details($cat->slug, $cat->name));
