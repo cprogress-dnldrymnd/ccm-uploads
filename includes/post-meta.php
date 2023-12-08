02cc78fb034ee227d5c90a2f01a292735b91cfc1
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-08 08:10:58 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -2022,7 +2022,7 @@ if (isset($_GET['post']) && is_admin()) {
 
 
 		global $wpdb;
-		$results = $wpdb->get_results("SELECT term_taxonomy_id FROM wp_term_relationships WHERE object_id = %p", $postid);
+		$results = $wpdb->prepare("SELECT term_taxonomy_id FROM wp_term_relationships WHERE object_id = %p", $postid);
 
 
 		var_dump($results);
