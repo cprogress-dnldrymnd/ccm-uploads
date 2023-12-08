9337ab5811bdb78c4101bc5a06b408094ab10f81
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-08 08:27:11 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -2030,7 +2030,7 @@ if (isset($_GET['post']) && is_admin()) {
 			ON term_relationships.term_taxonomy_id =  term_taxonomy.term_taxonomy_id AND 
 			 .parent = 405
 			INNER JOIN wp_termmeta as termmeta
-			ON wp_term_taxonomy.term_id = termmeta.term_id
+			ON wp_term_taxonomy.term_taxonomy_id = termmeta.term_id
 			WHERE term_relationships.object_id = $postid"
 		);
 		var_dump($results);
