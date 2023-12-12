f1f0e6e56b74f4f5e1c46b00fa9a69ccbfb714b0
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-12 17:12:14 +0800

    Update page-configure-bike-v2.php

--- templates/page-configure-bike-v2.php
+++ templates/page-configure-bike-v2.php
@@ -272,10 +272,8 @@ $bike_name = get_term($product_category, 'product_cat')->name;
                                 portalId: "139521183",
                                 formId: "7aa928aa-a966-4357-b74a-1f78c6b1c7c2",
                                 onFormReady: function($form) {
-                                    setTimeout(function() {
-                                        var $summary_items = 'ttttttt';
-                                        jQuery('input[name="lead_comments__ole_"]').val($summary_items).change();
-                                    }, 500);
+                                    var $summary_items = 'ttttttt';
+                                    jQuery('input[name="lead_comments__ole_"]').val($summary_items).change();
                                 }
                             });
                         </script>
