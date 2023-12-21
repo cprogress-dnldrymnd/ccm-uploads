2d2e4013b658895a4888d3739b3e1e07fb2a702b
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-21 18:53:07 +0800

    Update page-configure-bike-v2.php

--- templates/page-configure-bike-v2.php
+++ templates/page-configure-bike-v2.php
@@ -754,7 +754,7 @@ if (isset($_GET['action'])) {
             <?php } else { ?>
                 <?php foreach ($product_lists as $product) { ?>
                     jQuery('input[product_id="<?= $product ?>"]').click();
-                   // package_function(jQuery('input[product_id="<?= $product ?>"]'));
+                    package_function(jQuery('input[product_id="<?= $product ?>"]'));
                     
                 <?php } ?>
             <?php } ?>
