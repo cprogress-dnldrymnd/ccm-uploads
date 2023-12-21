f0dcd91580ba69f15989c4e180ff23ef26a4bfd6
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-21 17:42:42 +0800

    Update page-configure-bike-v2.php

--- templates/page-configure-bike-v2.php
+++ templates/page-configure-bike-v2.php
@@ -438,7 +438,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'create-post') {
         padding: 0;
         display: flex;
         align-items: center;
-        transition: 500ms;
+        transition: 300ms;
         max-width: 500px;
         width: 100%;
         left: 50%;
