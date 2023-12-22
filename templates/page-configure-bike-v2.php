0fbf7a40b8a15957782cf63130b8792af15afa9c
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-22 22:57:32 +0800

    Update page-configure-bike-v2.php

--- templates/page-configure-bike-v2.php
+++ templates/page-configure-bike-v2.php
@@ -840,9 +840,7 @@ if (isset($_GET['action'])) {
             jQuery($products_included).addClass('clicked');
             jQuery($products_included).click();
             unrelated_products_function(jQuery($products_included));
-        } else {
-            //unrelated_products_function(jQuery($products_included), true);
-        }
+        } 
         jQuery($products_included).addClass('disabled-unselect');
         $this.prop("checked", true).addClass('clicked');
 
