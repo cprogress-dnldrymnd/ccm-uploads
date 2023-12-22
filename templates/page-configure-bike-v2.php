2db1f2bcaaf5dfe43ea80da4ff782029cb99bcdb
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-22 22:57:21 +0800

    Update page-configure-bike-v2.php

--- templates/page-configure-bike-v2.php
+++ templates/page-configure-bike-v2.php
@@ -839,14 +839,13 @@ if (isset($_GET['action'])) {
         if (jQuery('.acc-option').hasClass('saved-data-loaded')) {
             jQuery($products_included).addClass('clicked');
             jQuery($products_included).click();
-
+            unrelated_products_function(jQuery($products_included));
         } else {
             //unrelated_products_function(jQuery($products_included), true);
         }
         jQuery($products_included).addClass('disabled-unselect');
         $this.prop("checked", true).addClass('clicked');
 
-        unrelated_products_function(jQuery($products_included));
 
     }
 
