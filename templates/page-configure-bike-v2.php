ad51e44bd16db8bd0fce57f16d38592d57cab30d
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-22 23:24:28 +0800

    Update page-configure-bike-v2.php

--- templates/page-configure-bike-v2.php
+++ templates/page-configure-bike-v2.php
@@ -792,7 +792,6 @@ if (isset($_GET['action'])) {
                 package_function(jQuery(this));
             }
             setTimeout(function() {
-                update_summary();
                 getTotal();
             }, 500);
         });
